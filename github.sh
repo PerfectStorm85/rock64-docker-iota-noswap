743f62b5b5e3af03bb86350f071eacdaff51c983
PerfectStorm85 <info@ronkamphuis.nl>
2018-07-20 18:51:46 +0000

    First submit

--- github.sh
+++ github.sh
@@ -3,6 +3,6 @@ read -p 'Changelist name: ' answer
 git init
 git add .
 git commit -m "$answer"
-git remote add origin https://github.com/PerfectStorm85/rock64-docker-iota.git
+git remote add noswap https://github.com/PerfectStorm85/rock64-docker-iota-noswap.git
 git remote -v
-git push -u origin master
+git push -u noswap master
